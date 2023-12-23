f828b6855447c5b0a6301da838d06aa27e5751e2
SahasraReddy22 <ch.en.u4cys21037@ch.students.amrita.edu>
2023-12-23 14:06:34 +0530

    first commit

---
+++ chapter3_letusc/young.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter the ages of 3 people");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a<b){
+        if(a<c){
+            printf("%d is smaller",a);
+        }else{
+            printf("%d is smaller",c);
+        }
+    }else{
+        if(b<c){
+            printf("%d is smaller",b);
+        }else{
+            printf("%d is smaller",c);
+        }
+    }
+    return 0;
+}
